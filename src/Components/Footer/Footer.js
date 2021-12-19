a4b33be2a126328d4dfddd2ff51ba52efc889906
Anik-Nath <anikdevnath03112000@gmail.com>
2021-12-19 23:36:10 +0600

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,27 +5,37 @@ const Footer = () => {
     return (
         <div className={styles.mainbg}>
            <div className="container text-white pt-5">
-               <div className="row">
+               <div className="row px-3 px-lg-0 px-md-0 py-3 pb-5">
                    <div className="col-lg-4 col-md-4 col-12">
-                        <h5 className="text-warning">
-                        <span className="h2 text-primary"><i className="fas fa-broom"></i></span>
-                            BD Cleener</h5>
-                        <p className="text-white-50">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, officia.</p>
+                        <h3 className="text-light">
+                        <span className="h2 text-primary"><i className="fas fa-broom text-warning"></i></span>
+                            Cleener</h3>
+                        <p className="text-white-50 py-3">We are a cleaning company that has been in the business for almost 7 years Since our inception we have built a formidable reputation as a high-quality.</p>
+                      <div className="d-flex gap-3 fs-4">
+                        <i className="fab fa-facebook text-white-50"></i>
+                        <i className="fab fa-instagram text-white-50"></i>
+                        <i className="fab fa-linkedin text-white-50"></i>
+                        <i className="fab fa-github text-white-50"></i>
+                      </div>
                    </div>
-                   <div className="col-lg-4 col-md-4 col-12">
-                       <h5>UseFul Link</h5>
+                   <div className="col-lg-4 col-md-4 col-12 my-5 my-lg-0 my-md-0">
+                       <h5 className="pb-4">UseFul Link</h5>
                        <ul className="text-white-50 list-unstyled">
-                           <li>Link 1</li>
-                           <li>Link 1</li>
-                           <li>Link 1</li>
+                           <li><i className="fas fa-chevron-right me-2"></i> About Us</li>
+                           <li><i className="fas fa-chevron-right me-2 py-2"></i> Services</li>
+                           <li><i className="fas fa-chevron-right me-2"></i> Testimonial</li>
+                           <li><i className="fas fa-chevron-right me-2 py-2"></i> Pricing</li>
+                           <li><i className="fas fa-chevron-right me-2"></i> Appoinment</li>
                        </ul>
                    </div>
                    <div className="col-lg-4 col-md-4 col-12">
-                   <h5>UseFul Link</h5>
+                   <h5 className="pb-4">Our Services</h5>
                     <ul className="text-white-50 list-unstyled">
-                        <li>Link 1</li>
-                        <li>Link 1</li>
-                        <li>Link 1</li>
+                        <li><i className="fas fa-chevron-right me-2"></i>Windows Cleaning Service</li>
+                        <li><i className="fas fa-chevron-right me-2 py-2"></i>Bathroom Cleaning Service</li>
+                        <li><i className="fas fa-chevron-right me-2"></i>Computer Cleaning Service</li>
+                        <li><i className="fas fa-chevron-right me-2 py-2"></i>Express Cleaning Service</li>
+                        <li><i className="fas fa-chevron-right me-2"></i>Carpet Cleaning Service</li>
                     </ul>
                    </div>
                </div><hr />
