a4107b7cb0361f6adbef69ecb5bb704a9e8d8d73
Anik-Nath <anikdevnath03112000@gmail.com>
2021-12-19 23:11:01 +0600

    map added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,9 +4,9 @@ import styles from "../Navigation/myButton.module.css";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="">
       <div className={styles.pageBackground}>
-        <div className="container py-5">
+        <div className="container py-5 mb-5">
           <div className="row py-5">
             <div className="col-12 text-center text-light py-5">
               <h1 className="display-3">Contact</h1>
@@ -15,8 +15,16 @@ const Contact = () => {
         </div>
       </div>
       <MakeAppoinment></MakeAppoinment>
+
+      <div class="">
+        <div class="py-5 mb-5">
+          <iframe style={{width:"100%",height:"450px",border:"0"}} title="location"  src="https://maps.google.com/maps?q=chittagong,bangldesh&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+            </div>
+          </div>
     </div>
   );
 };
 
 export default Contact;
+
